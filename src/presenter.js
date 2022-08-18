9e3953dd3461013e9903fc06a284b878b8dd58b1
E-Doggo <94150885+E-Doggo@users.noreply.github.com>
2022-08-18 08:50:40 -0400

    Se anadio la funcionalidad de saludar por dia

--- src/presenter.js
+++ src/presenter.js
@@ -1,4 +1,4 @@
-//import sumar from "./sumador";
+import saludar from "./sumador";
 
 const nombre = document.querySelector("#nombre");
 const form = document.querySelector("#formSaludador");
@@ -6,9 +6,7 @@ const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-
-  const nombre_Usuario = nombre.value;
-  console.log(nombre_Usuario);
-
-  div.innerHTML = "<p>"+"Hola"+nombre_Usuario+"</p>"; 
+  let fecha = new Date()
+  let hora = fecha.getHours();
+  div.innerHTML = "<p>"+saludar(nombre, hora)+"</p>"; 
 });
--- src/sumador.js
+++ src/sumador.js
@@ -1,5 +1,24 @@
-function sumar(a, b) {
-  return a + b;
+function getHora(hora) {
+  let buen= ""
+  if (hora >0 && hora <12)
+  {
+    buen+= "Buenos dias "
+  }
+  else if (hour>=12 && hour<=18)
+  {
+    buen+="Buenas tardes "
+  }
+  else
+  {
+    buen+="Buenas Noches "
+  }
+  return buen
 }
 
-export default sumar;
+function saludar(nombre, hora)
+{
+  const nombre_Usuario = nombre.value;
+  
+  return  getHora(hora)+ nombre_Usuario; 
+}
+export default saludar;
