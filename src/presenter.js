69cc909b597d733284e16e6b26c2956d545b50f0
E-Doggo <94150885+E-Doggo@users.noreply.github.com>
2022-08-18 08:38:57 -0400

    Se anadio la funcion saludar por nombre

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,14 @@
-import sumar from "./sumador";
+//import sumar from "./sumador";
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
+const nombre = document.querySelector("#nombre");
+const form = document.querySelector("#formSaludador");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
+  const nombre_Usuario = nombre.value;
+  console.log(nombre_Usuario);
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  div.innerHTML = "<p>"+"Hola"+nombre_Usuario+"</p>"; 
 });
